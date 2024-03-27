f2a8b80ebfd2b056d0dad4a1946e3a3518402656
arc363 <arc3363@yahoo.co.uk>
2024-03-27 15:01:16 +0000

    Comments added to program

--- semantic.py
+++ semantic.py
@@ -4,6 +4,7 @@ nlp = spacy.load('en_core_web_md')
 
 print("-------- Practical Task 1 --------\n")
 
+# Compare words for semantic similarity
 word1 = nlp('cat')
 word2 = nlp('monkey')
 word3 = nlp('banana')
@@ -12,10 +13,15 @@ print(word1, word2, word3)
 print(round(word1.similarity(word2), 3))
 print(round(word1.similarity(word3), 3))
 print(round(word2.similarity(word3), 3))
+
+# My observations on the comparison
 print("Comment: as noted in the documentation, the highest similarity is "
       "between the two animals; \"banana\" is more similar to \"monkey\" than "
       "to \"cat\". ")
 print()
+
+
+# Compare three other words for semantic similarity
 word1 = nlp('jump')
 word2 = nlp('think')
 word3 = nlp('sprint')
@@ -23,6 +29,8 @@ print(word1, word2, word3)
 print(round(word1.similarity(word2), 3))
 print(round(word1.similarity(word3), 3))
 print(round(word2.similarity(word3), 3))
+
+# My observations on the comparison
 print("Comment: the highest similarity is between the verbs \"jump\" and "
       "\"think\", which are closer semantically than either verb and \"think\""
       ".")
@@ -33,15 +41,18 @@ print("\nComment: comparing the complaints vs recipes example with two "
       "semantic similarity between texts may be underestimated and the "
       "program's performance affected.\n")
 
+# Working with vectors
 print("Working with vectors")
 
 tokens = nlp('water sea calm liquid')
 
+# Loops through list of words and compares each with the others
 for token1 in tokens:
     for token2 in tokens:
         print(f"{token1.text:{8}} {token2.text:{8}} {round(token1.similarity(token2), 3)}")
         
 
+# Working with sentences
 print("\nWorking with sentences")
 sentence_to_compare = "Why is my cat on the car"
 
@@ -54,6 +65,7 @@ sentences = ["Where did my dog go",
 
 model_sentence = nlp(sentence_to_compare)
 
+# Loops through list of sentenes, scoring them for similarity to the main sentence
 for sentence in sentences:
     similarity = round(nlp(sentence).similarity(model_sentence), 3)
     print(f"{sentence:{30}} {str(similarity)}")
